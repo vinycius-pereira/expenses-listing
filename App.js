bf990173dd087f386bc62f78669575b662bebcf9
Vinícius Pereira <vinicius.pereira@waybee.com.br>
2023-05-03 20:06:37 -0300

    Added screens and navigation

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import AllExpenses from "./screens/AllExpenses";
+import ManageExpenses from "./screens/ManageExpenses";
+import RecentExpenses from "./screens/RecentExpenses";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator()
+const BottomTab = createMaterialBottomTabNavigator()
+
+function ExpensesOverview(){
+    return (
+        <BottomTab.Navigator>
+            <BottomTab.Screen name="Recent Expenses" component={RecentExpenses}/>
+            <BottomTab.Screen name="All Expenses" component={AllExpenses}/>
+        </BottomTab.Navigator>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+    return (
+        <>
+            <StatusBar style="auto"/>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Expenses Overview" component={ExpensesOverview}/>
+                    <Stack.Screen name="Manage Expenses" component={ManageExpenses}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
+}
---
+++ screens/ManageExpenses.js
@@ -0,0 +1,6 @@
+import {Text} from 'react-native'
+function ManageExpenses() {
+    return <Text>ManageExpenses</Text>
+}
+
+export default ManageExpenses
