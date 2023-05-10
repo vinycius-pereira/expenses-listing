364cd5e03ba1905d6af6dd0a06d6fadb2e52169a
Vinícius Pereira <vinicius.pereira@waybee.com.br>
2023-05-10 19:34:43 -0300

    changed dummy variables to context variables

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,46 +1,18 @@
-import {View, StyleSheet} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import {GlobalStyles} from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-    {
-        id: 'e1',
-        description: 'A pair of shoes',
-        amount: 59.99,
-        date: new Date('2023-05-03')
-    },
-    {
-        id: 'e2',
-        description: 'A pair of trousers',
-        amount: 30.99,
-        date: new Date('2023-06-18')
-    },
-    {
-        id: 'e3',
-        description: 'A pair of glasses',
-        amount: 199.99,
-        date: new Date('2023-01-04')
-    },
-    {
-        id: 'e4',
-        description: 'Some Magic the gathering cards',
-        amount: 85.39,
-        date: new Date('2023-03-16')
-    },
-    {
-        id: 'e5',
-        description: 'A Playmat',
-        amount: 19.99,
-        date: new Date('2023-10-06')
-    }
-]
 
-function ExpensesOutput({expenses, expensesPeriod}) {
+function ExpensesOutput({expenses, expensesPeriod, fallbackText}) {
     return (
         <View style={styles.container}>
-            <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod}/>
-            <ExpensesList expenses={DUMMY_EXPENSES}/>
+            <ExpensesSummary expenses={expenses} periodName={expensesPeriod}/>
+            {expenses.length > 0 ? (
+                <ExpensesList expenses={expenses}/>
+            ) : (
+                <Text style={styles.infoText}>{fallbackText}</Text>
+            )}
         </View>
     )
 }
@@ -54,5 +26,11 @@ const styles = StyleSheet.create({
         paddingTop: 24,
         paddingBottom: 12,
         backgroundColor: GlobalStyles.colors.primary700
+    },
+    infoText: {
+        color: "white",
+        fontSize: 16,
+        textAlign: "center",
+        marginTop: 32
     }
 })
