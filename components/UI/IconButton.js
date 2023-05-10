ca4cd6b4ddcfd49d1f6a48c069bfc8575cc53910
Vinícius Pereira <vinicius.pereira@waybee.com.br>
2023-05-10 19:33:03 -0300

    created generic button component

---
+++ components/UI/IconButton.js
@@ -0,0 +1,26 @@
+import {StyleSheet, Pressable, View} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+function IconButton({icon, size, color, onPress}) {
+    return (
+        <Pressable onPress={onPress} style={({pressed}) => pressed && styles.pressed}>
+            <View style={styles.buttonContainer}>
+                <Ionicons name={icon} size={size} color={color}/>
+            </View>
+        </Pressable>
+    )
+}
+
+export default IconButton
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        borderRadius: 24,
+        padding: 6,
+        marginHorizontal: 14,
+        marginVertical: 2
+    },
+    pressed: {
+        opacity: 0.75
+    }
+})
