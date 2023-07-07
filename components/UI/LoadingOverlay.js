e9039a1e1f9406e962ad793002c4b056c92331fc
Vinícius Pereira <vinicius.pereira@waybee.com.br>
2023-07-06 22:34:43 -0300

    fixed loading size

--- components/UI/LoadingOverlay.js
+++ components/UI/LoadingOverlay.js
@@ -11,9 +11,9 @@ export default LoadingOverlay
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        height: '100%',
         padding: 24,
         backgroundColor: GlobalStyles.colors.primary700
     }
